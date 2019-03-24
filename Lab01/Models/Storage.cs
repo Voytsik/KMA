656f1ce68880e6ba0533994cb9103f1dfcff3214
Voytsik <48546238+Voytsik@users.noreply.github.com>
2019-03-24 22:07:15 +0200

    Update Storage.cs

--- Lab01/Models/Storage.cs
+++ Lab01/Models/Storage.cs
@@ -1,11 +1,14 @@
-﻿using System;
+using System;
 
-namespace Lab01.Models {
-    public class Storage {
-        public event Action<Basic> InfoChanged;
-        public Basic Info { get; private set; }
+namespace Lab01.Models
+{
+    public class Storage
+    {
+        public event Action<Info> InfoChanged;
+        public Info Info { get; private set; }
 
-        public void ChangeInfo(Basic info) {
+        public void ChangeInfo(Info info)
+        {
             Info = info;
             InfoChanged?.Invoke(info);
         }

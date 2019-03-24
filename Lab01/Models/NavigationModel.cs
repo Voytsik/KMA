090759d82c1ab1a802bb05022df7967d363edc86
Voytsik <48546238+Voytsik@users.noreply.github.com>
2019-03-24 22:07:05 +0200

    Update NavigationModel.cs

--- Lab01/Models/NavigationModel.cs
+++ Lab01/Models/NavigationModel.cs
@@ -1,16 +1,20 @@
-﻿using Lab01.Views;
+using Lab01.Views;
 using Lab01.Windows;
 
-namespace Lab01.Models {
-    class NavigationModel {
+namespace Lab01.Models
+{
+    class NavigationModel
+    {
         private ContentWindow _contentWindow;
         private LoginView _loginView;
 
-        public NavigationModel(ContentWindow contentWindow, Storage storage) {
+        public NavigationModel(ContentWindow contentWindow, Storage storage)
+        {
             _contentWindow = contentWindow;
             _loginView = new LoginView(storage);
         }
-        public void Navigate() {
+        public void Navigate()
+        {
             _contentWindow.ContentControl.Content = _loginView;
         }
     }

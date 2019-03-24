244dd30bcdb8483e49c1cdf2f1f25fac853a84dd
Voytsik <48546238+Voytsik@users.noreply.github.com>
2019-03-24 22:06:55 +0200

    Update LoginModel.cs

--- Lab01/Models/LoginModel.cs
+++ Lab01/Models/LoginModel.cs
@@ -1,79 +1,147 @@
-﻿using Lab01.Manager;
+using Lab01.Manager;
 using System;
 using System.Windows;
 
-namespace Lab01.Models {
-    class LoginModel {
-        private readonly Storage _storage;
-        public event Action<Basic> UInfoChanged;
+namespace Lab01.Models
+{
+    class LoginModel
+    {
+        public readonly Storage _storage;
+        public event Action<Info> UInfoChanged;
 
-        public LoginModel(Storage storage) {
+        public LoginModel(Storage storage)
+        {
             _storage = storage;
             storage.InfoChanged += OnInfoChanged;
         }
 
-        private void OnInfoChanged(Basic info) {
+        private void OnInfoChanged(Info info)
+        {
             UInfoChanged?.Invoke(info);
         }
 
-        bool isBirthday(DateTime date) {
-            if (DateTime.Today.Date.Day == date.Date.Day && DateTime.Today.Date.Month == date.Date.Month) return true;
-            else return false;
-        }
-
-        public void CheckDate(DateTime date) {
-            var age = DateTime.Today.Date.Year - date.Date.Year;
-            if (date > DateTime.Today.AddYears(-age)) age--;
-            if (age < 0) MessageBox.Show("Помилка. Ви ще не народилися.");
-            else if (age >= 135) MessageBox.Show("Помилка. Ви вже повинні були померти.");
-            else {
-                if (isBirthday(date.Date)) MessageBox.Show("Вітаємо з днем народження!");
-                Basic info = new Basic {
-                    Age = age,
-                    WesternZodiac = FindWestZodiac(date),
-                    ChineseZodiac = FindChineseZodiac(date.Year),
-                    isBirthdayToday = isBirthday(date.Date)
-                };
-                
-                _storage.ChangeInfo(info);
+        public bool isBirthday(DateTime date)
+        {
+            if (DateTime.Today.Date.Day == date.Date.Day && DateTime.Today.Date.Month == date.Date.Month)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
             }
         }
 
-        private string FindWestZodiac(DateTime date) {
+        enum WestZodiacSigns
+        {
+            Aries = 1,
+            Taurus,
+            Gemini,
+            Cancer,
+            Leo,
+            Virgo,
+            Libra,
+            Scorpio,
+            Sagittarius,
+            Capricorn,
+            Aquarius,
+            Pisces
+        };
+
+        public string FindWestZodiac(DateTime date) {
             var day = date.Day;
             var month = date.Month;
-            if ((month == 3 && day >= 21) || (month == 4 && day <= 20)) return "Овен";
-            if ((month == 4 && day >= 21) || (month == 5 && day <= 20)) return "Тілець";
-            if ((month == 5 && day >= 21) || (month == 6 && day <= 21)) return "Близнюки";
-            if ((month == 6 && day >= 22) || (month == 7 && day <= 22)) return "Рак";
-            if ((month == 7 && day >= 23) || (month == 8 && day <= 23)) return "Лев";
-            if ((month == 8 && day >= 24) || (month == 9 && day <= 23)) return "Діва";
-            if ((month == 9 && day >= 24) || (month == 10 && day <= 22)) return "Терези";
-            if ((month == 10 && day >= 23) || (month == 11 && day <= 22)) return "Скорпіон";
-            if ((month == 11 && day >= 23) || (month == 12 && day <= 21)) return "Стрілець";
-            if ((month == 12 && day >= 22) || (month == 1 && day <= 20)) return "Козеріг";
-            if ((month == 1 && day >= 21) || (month == 2 && day <= 19)) return "Водолій";
-            if ((month == 2 && day >= 20) || (month == 3 && day <= 20)) return "Риби";
-            return "Error!";
+            WestZodiacSigns Zodiac;
+            if ((month == 3 && day >= 21) || (month == 4 && day <= 20))
+            {
+                Zodiac = WestZodiacSigns.Aries;
+            }
+            else if ((month == 4 && day >= 21) || (month == 5 && day <= 20))
+            {
+                Zodiac = WestZodiacSigns.Taurus;
+            }
+            else if ((month == 5 && day >= 21) || (month == 6 && day <= 21))
+            {
+                Zodiac = WestZodiacSigns.Gemini;
+            }
+            else if ((month == 6 && day >= 22) || (month == 7 && day <= 22))
+            {
+                Zodiac = WestZodiacSigns.Cancer;
+            }
+            else if ((month == 7 && day >= 23) || (month == 8 && day <= 23))
+            {
+                Zodiac = WestZodiacSigns.Leo;
+            }
+            else if ((month == 8 && day >= 24) || (month == 9 && day <= 23))
+            {
+                Zodiac = WestZodiacSigns.Virgo;
+            }
+            else if ((month == 9 && day >= 24) || (month == 10 && day <= 22))
+            {
+                Zodiac = WestZodiacSigns.Libra;
+            }
+            else if ((month == 10 && day >= 23) || (month == 11 && day <= 22))
+            {
+                Zodiac = WestZodiacSigns.Scorpio;
+            }
+            else if ((month == 11 && day >= 23) || (month == 12 && day <= 21))
+            {
+                Zodiac = WestZodiacSigns.Sagittarius;
+            }
+            else if((month == 12 && day >= 22) || (month == 1 && day <= 20))
+            {
+                Zodiac = WestZodiacSigns.Capricorn;
+            }
+            else if((month == 1 && day >= 21) || (month == 2 && day <= 19))
+            {
+                Zodiac = WestZodiacSigns.Aquarius;
+            }
+            else if ((month == 2 && day >= 20) || (month == 3 && day <= 20))
+            {
+                Zodiac = WestZodiacSigns.Pisces;
+            }
+            else
+            {
+                return "Error!";
+            }
+            return Zodiac.ToString();
         }
 
-        private string FindChineseZodiac(int year) {
+        enum AsianZodiacSigns
+        {
+            Monkey = 1,
+            Rooster,
+            Dog,
+            Pig,
+            Rat,
+            Ox,
+            Tiger,
+            Rabbit,
+            Dragon,
+            Snake,
+            Horse,
+            Ram
+        };
+
+        public string FindChineseZodiac(int year) {
             var modY = year % 12;
+            AsianZodiacSigns Zodiac;
             switch (modY) {
-                case 0: return "Мавпа";
-                case 1: return "Півень";
-                case 2: return "Собака";
-                case 3: return "Свиня";
-                case 4: return "Пацюк";
-                case 5: return "Бик";
-                case 6: return "Тигр";
-                case 7: return "Кролик";
-                case 8: return "Дракон";
-                case 9: return "Змія";
-                case 10: return "Кінь";
-                case 11: return "Вівця";
+                case 0: Zodiac = AsianZodiacSigns.Monkey; break;
+                case 1: Zodiac = AsianZodiacSigns.Rooster; break;
+                case 2: Zodiac = AsianZodiacSigns.Dog; break;
+                case 3: Zodiac = AsianZodiacSigns.Pig; break;
+                case 4: Zodiac = AsianZodiacSigns.Rat; break;
+                case 5: Zodiac = AsianZodiacSigns.Ox; break;
+                case 6: Zodiac = AsianZodiacSigns.Tiger; break;
+                case 7: Zodiac = AsianZodiacSigns.Rabbit; break;
+                case 8: Zodiac = AsianZodiacSigns.Dragon; break;
+                case 9: Zodiac = AsianZodiacSigns.Snake; break;
+                case 10: Zodiac = AsianZodiacSigns.Horse; break;
+                case 11: Zodiac = AsianZodiacSigns.Ram; break;
                 default: return "Error!";
             }
+            return Zodiac.ToString();
         }
     }
 }

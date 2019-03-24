d50aa88b32a00294bf70767c35021456b8bb9c6b
Voytsik <48546238+Voytsik@users.noreply.github.com>
2019-03-24 22:10:42 +0200

    Update LoginView.xaml.cs

--- Lab01/Views/LoginView.xaml.cs
+++ Lab01/Views/LoginView.xaml.cs
@@ -1,12 +1,15 @@
-﻿using Lab01.Models;
+using Lab01.Models;
 using Lab01.ViewModels;
 using System.Windows.Controls;
 
-namespace Lab01.Views {
-    public partial class LoginView : UserControl {
+namespace Lab01.Views
+{
+    public partial class LoginView : UserControl
+    {
         private LoginViewModel _viewModel;
 
-        public LoginView(Storage storage) {
+        public LoginView(Storage storage)
+        {
             InitializeComponent();
             _viewModel = new LoginViewModel(storage);
             DataContext = _viewModel;

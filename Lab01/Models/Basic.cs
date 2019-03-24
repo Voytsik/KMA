316d2ed67ae20d9eaeff289eaa9d80f6b848e83b
Voytsik <48546238+Voytsik@users.noreply.github.com>
2019-03-24 22:06:45 +0200

    Update and rename Basic.cs to Info.cs

--- Lab01/Models/Basic.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Lab01.Models {
-    public class Basic {
-        public int Age { get; set; }
-        public bool isBirthdayToday { get; set; }
-        public bool isNotFirst { get; set; }
-        public string WesternZodiac { get; set; }
-        public string ChineseZodiac { get; set; }
-    }
-}
---
+++ Lab01/Models/Info.cs
@@ -0,0 +1,11 @@
+namespace Lab01.Models
+{
+    public class Info
+    {
+        public int Age { get; set; }
+        public bool IsBirthdayToday { get; set; }
+        public bool IsNotFirst { get; set; }
+        public string WesternZodiac { get; set; }
+        public string ChineseZodiac { get; set; }
+    }
+}
